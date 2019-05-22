51aa3d312a06ec1ee610444b336ebbcdaf580253
Ajar Vashisth <ajar.vashisth@dexcom.com>
2019-05-22 10:41:43 -0700

    code

--- src/app.py
+++ src/app.py
@@ -1,49 +1,56 @@
+from flask import Flask,request, Response
+import requests
+import kafka_client
 import json
-import asyncio
 import sys
-import aiohttp
-from flask import Flask, current_app,Response
 
-from flask_aiohttp import AioHTTP
+
+APP_CONFIG_FILE = '/appcfg/rest_service_config.json'
+with open(APP_CONFIG_FILE) as f:
+  APP_CONFIG = json.load(f)
+
+_KafkaProducer = kafka_client.KafkaProducer(APP_CONFIG['kafka_config'])
+REST_BACKEND_HOST_PORT = APP_CONFIG['rest_proxy']['host_port']
 
 app = Flask(__name__)
 
-aio = AioHTTP(app)
 
-REST_SERVICE_HOST_PORT = sys.environ['REST_SERVICE_HOST_PORT']
-
-
-@async
 def _proxy(*args, **kwargs):
-    response = yield from aiohttp.request(
+    resp = requests.request(
         method=request.method,
-        url=request.url.replace(request.host_url, REST_SERVICE_HOST_PORT),
+        url=request.url.replace(request.host_url, REST_BACKEND_HOST_PORT),
         headers={key: value for (key, value) in request.headers if key != 'Host'},
         data= kwargs.get('data', request.get_data()),
         cookies=request.cookies,
         allow_redirects=False)
 
-    resp_data = yield from response.read()
-    status_code = response.status_code
-    
     excluded_headers = ['content-encoding', 'content-length', 'transfer-encoding', 'connection']
-    headers = [(name, value) for (name, value) in response.raw.headers.items()
+    headers = [(name, value) for (name, value) in resp.raw.headers.items()
                if name.lower() not in excluded_headers]
 
-    return (resp_data, status_code, headers)
+    return (resp.content, resp.status_code, headers)
 
-
-@app.route('/', defaults={'path': ''},methods=['GET','POST'])
-@app.route('/<path:path>',methods=['GET','POST'])
-@async
-def api():
-    (resp_data, status_code, headers) = yield from _proxy()
+@app.route('/topics/<string:topic>',methods=['POST'])
+def topics_post(topic):
+    app.logger.info(f'request - {request.remote_addr} {request.method} {request.path}')
+    request.on_json_loading_failed = lambda e: ({"error":f"Request data is not good JSON - {e}"})
+    payload = request.get_json()
+    records = payload['records']
+    headers = dict(remote_ip=request.remote_addr)
+    responses = _KafkaProducer.send_records(topic,records,headers)
+    response = Response(json.dumps(responses))
+    return response
+    
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def proxy(path):
+    print('hi')
+    app.logger.info(f'request - {request.remote_addr} {request.method} {request.path}')
+    content,status_code,headers = _proxy()
+    print(content,status_code,headers)
     response = Response(content, status_code, headers)
     return response
 
-
-def main():
-    aio.run(app, debug=True)
-
 if __name__ == '__main__':
-    main()
+  print('listening')
+  app.run(host='0.0.0.0', port=8080)
---
+++ src/kafka_client.py
@@ -0,0 +1,49 @@
+import datetime
+import time
+import sys
+import yaml
+import json
+
+from confluent_kafka import Producer,Consumer,KafkaError
+
+from collections import defaultdict
+import argparse
+import traceback
+import logging
+
+LOGGER = logging.getLogger(__file__)
+
+
+
+class KafkaProducer:
+    def __init__(self,conf):
+        self.producer = Producer(conf)
+    def send_records(self,topic,records,headers):
+        responses = []
+        def delivery_report(err, msg):
+            """ Called once for each message produced to indicate delivery result.
+                Triggered by poll() or flush(). """
+            if err is not None:
+                LOGGER.debug('Message delivery failed: {}'.format(err))
+            else:
+                LOGGER.debug('Message delivered {} {} {} [{}] {}'.format( msg.timestamp(),msg.offset(), msg.topic(), msg.partition(), msg.key()))
+
+            response=dict(error = f"{err}" if err else None, 
+                report=dict(timestamp=msg.timestamp(),partition=msg.partition(),offset=msg.offset(),key=msg.key()))
+            responses.append(response)
+ 
+        for record in records:
+            data = json.dumps(record["value"])
+            key = record.get('key')
+            self.producer.produce(topic, data, key=key, callback=delivery_report,headers=headers)
+        
+        try:
+            self.producer.poll(0)
+        except:
+            traceback.print_exc()
+        self.producer.flush()
+        return responses
+
+if __name__ == '__main__':
+    _KP = KafkaProducer({'bootstrap.servers':'libra:9092'})
+
