e4a897ecf7286c5cd5188d0efe3dde60ec1101b5
Ajar Vashisth <ajar.vashisth@dexcom.com>
2019-05-23 22:47:33 -0700

    partition

--- app/app/main.py
+++ app/app/main.py
@@ -29,7 +29,6 @@ def _proxy(*args, **kwargs):
 @app.route('/offsets/<string:topic>',methods=['GET'])
 def topic_offsets(topic):
     app.logger.info(f'request - {request.remote_addr} {request.method} {request.path}')
-    LOGGER.info(f"payload -{request.is_json} {payload} ")
     response_data = _KafkaConsumer.get_topic_offsets(topic)
     response = Response(json.dumps(response_data))
     return response
